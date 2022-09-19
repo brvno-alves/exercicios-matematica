1a7d46b544d73cfbedb3b75aec1e7b467bb87735
Bruno R C Alves <bruno-rca@hotmail.com>
2022-09-19 10:49:32 -0300

    comit inicial

---
+++ Tabuada.java
@@ -0,0 +1,11 @@
+public class Tabuada {
+    public static void main(String[] args) {
+       for(int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+           System.out.println("Tabuada do " + multiplicador);
+           for(int contador = 0; contador<=10; contador++) {
+               int resultado = contador * multiplicador;
+               System.out.println("  " + multiplicador + " * " + contador + " = " + resultado);
+           }
+       }
+   }
+}
